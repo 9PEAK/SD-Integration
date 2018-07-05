90c6fcc427c2c05a0f56e6695b06060eec4ecaf5
Kane <52653784@qq.com>
2018-07-05 08:24:22 +0800

    Start

---
+++ src/SDProduct/Model/SDProduct.php
@@ -0,0 +1,54 @@
+<?php
+namespace Peak\SD\SDProduct\Model;
+
+trait Product {
+
+
+	protected static $sd_pref = 'sd';
+	protected static $sd_exp = 60*6;
+
+	protected static function sd_key ($id)
+	{
+		if ( $id) {
+			return self::$sd_pref.'-'.static::class.'-'.$id;
+		}
+	}
+
+
+	public function sdProduct()
+	{
+		return $this->sdProduct = Cache::remember(
+			self::sd_key($this->id),
+			self::$sd_exp,
+			function () {
+				$api = new \Peak\MicroService\Integration\SDProduct (
+					[
+						'token' => [
+							'id' => 'sd-product',
+							'key' => config('sd.sd-product'),
+						]
+					],
+					[
+//				'api_url' => 'http://sd-laravel/product/'
+						'api_url' => 'http://sd.9peak.net/product/'
+					]
+				);
+
+				$res = $api->request(
+					'searchProduct',
+					[
+						'id' => 1
+					]
+				);
+
+				if ($res) {
+					return $api->result[0];
+				}
+			}
+		);
+	}
+
+
+
+
+}
